0e8f665c465533d555219c46f48d48074fd30db3
ZG928 <3132057704@qq.com>
2025-10-17 21:53:42 +0800

    Add testSubtraction (will fail)

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,14 +1,17 @@
-package org.example;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
-
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    public void testAddition() {
+        Calc c = new Calc();
+        assertEquals(5, c.add(2, 3));
     }
 
+    // 新增的测试方法
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2)); // 预期4-2=2，若subtract方法错误则失败
+    }
 }
